419857bd805ae536337555b4110ef0d6dc8270ed
tinhinane <114427567+tinyaitmess@users.noreply.github.com>
2023-11-22 18:06:26 +0100

    Create list.h

---
+++ list.h
@@ -0,0 +1,21 @@
+#ifndef LIST_H
+#define LIST_H
+
+
+#include "etat.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Noeud Noeud;
+
+typedef struct l Liste;
+
+void initialiserListe(Liste *liste);
+void ajouterDebutListe(Liste *liste, ptrEtat valeur);
+void enleverFinListe(Liste *liste);
+bool listeContient(const Liste *liste, const ptrEtat etat);
+void afficherListe(const Liste *liste);
+void libererListe(Liste *liste);
+
+
+#endif
